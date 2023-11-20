3beda0972f1c0c62d38744148b01071e97812b18
Addison Baum <70862672+addycb@users.noreply.github.com>
2023-11-19 21:12:28 -0500

    Added idt entry def

--- types.h
+++ types.h
@@ -36,3 +36,11 @@ struct InterruptDescriptor32 {
    uint8_t  type_attributes; // gate type, dpl, and p fields
    uint16_t offset_2;        // offset bits 16..31
 };
+
+typedef struct {
+	uint16_t    isr_low;      // The lower 16 bits of the ISR's address
+	uint16_t    kernel_cs;    // The GDT segment selector that the CPU will load into CS before calling the ISR
+	uint8_t     reserved;     // Set to zero
+	uint8_t     attributes;   // Type and attributes; see the IDT page
+	uint16_t    isr_high;     // The higher 16 bits of the ISR's address
+} __attribute__((packed)) idt_entry_t;
