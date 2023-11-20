83a955fdcd62a1fa8f6e55024bf2df28c45d7e26
Addison Baum <70862672+addycb@users.noreply.github.com>
2023-11-19 21:42:59 -0500

    Fixing header file location

--- types.h
+++ types.h
@@ -28,19 +28,3 @@ typedef uint16 uint16_t;
 typedef uint32 uint32_t;
 typedef uint64 uint64_t;
 #endif
-
-struct InterruptDescriptor32 {
-   uint16_t offset_1;        // offset bits 0..15
-   uint16_t selector;        // a code segment selector in GDT or LDT
-   uint8_t  zero;            // unused, set to 0
-   uint8_t  type_attributes; // gate type, dpl, and p fields
-   uint16_t offset_2;        // offset bits 16..31
-};
-
-typedef struct {
-	uint16_t    isr_low;      // The lower 16 bits of the ISR's address
-	uint16_t    kernel_cs;    // The GDT segment selector that the CPU will load into CS before calling the ISR
-	uint8_t     reserved;     // Set to zero
-	uint8_t     attributes;   // Type and attributes; see the IDT page
-	uint16_t    isr_high;     // The higher 16 bits of the ISR's address
-} __attribute__((packed)) idt_entry_t;
